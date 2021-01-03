eef2882e6666e45819ee6aa1e38118587f199df6
rdmoll <ryan.d.moll@gmail.com>
2021-01-03 15:06:08 -0500

    Updates to simple advection-diffusion test

--- main.cpp
+++ main.cpp
@@ -41,41 +41,54 @@ int main( int argc, const char * argv[] )
   
   std::vector< double > T1_phys( Nx, 0.0 );
   std::vector< std::complex< double > > T_spec( nOutX, std::complex< double >( 0.0, 0.0 ) );
+  std::vector< std::vector< double > > T_write( Nx, std::vector< double >( Ny, 0.0 ) );
   
   hydroCode::FourierTransforms fft;
+  io::ioNetCDF testWriter( "/Users/rmoll/Desktop/test_AdvDiff_xy.nc", "data", Nx, Ny );
   
-  fft.fft_r2c( T0_phys, T_spec );
-  
-  for( size_t j = 0; j < nSteps; j++ )
-  {
-    for( size_t i = 1; i < nOutX; i++ )
-    {
-      T_spec[ i ] /= ( 1.0 + fac1 * 2.0 * pi * i * std::complex< double >( 0.0, 1.0 )
-                           + fac2 * 4.0 * pi * pi * i * i );
-    }
-  }
-  
-  fft.fft_c2r( T_spec, T1_phys );
-  
-  for( int i = 0; i < Nx; i++ )
-  {
-    std::cout << T0_phys[ i ] << " --> " << T1_phys[ i ] / 16.0 << std::endl;
-  }
-  
-  std::vector< std::vector< double > > T_write( Nx, std::vector< double >( Ny, 0.0 ) );
   for( int i = 0; i < Nx; i++ )
   {
     for( int j = 0; j < Ny; j++ )
     {
-      T_write[ i ][ j ] = T1_phys[ i ];
+      T_write[ i ][ j ] = T0_phys[ i ];
     }
   }
-  
-  // Write netCDF data
-  io::ioNetCDF testWriter( "/Users/rmoll/Desktop/test_AdvDiff_xy.nc", "data", Nx, Ny );
   testWriter.write( 0, T_write );
-  testWriter.write( 1, T_write );
-  testWriter.write( 2, T_write );
+  
+  // Starting time step loop
+  for( size_t t = 0; t < 4; t++ )
+  {
+    fft.fft_r2c( T0_phys, T_spec );
+    
+    for( size_t j = 0; j < nSteps; j++ )
+    {
+      for( size_t i = 1; i < nOutX; i++ )
+      {
+        T_spec[ i ] /= ( 1.0 + fac1 * 2.0 * pi * i * std::complex< double >( 0.0, 1.0 )
+                             + fac2 * 4.0 * pi * pi * i * i );
+      }
+    }
+    
+    fft.fft_c2r( T_spec, T1_phys );
+    
+    //for( int i = 0; i < Nx; i++ )
+    //{
+    //  std::cout << T0_phys[ i ] << " --> " << T1_phys[ i ] / 16.0 << std::endl;
+    //}
+    
+    for( int i = 0; i < Nx; i++ )
+    {
+      T0_phys[ i ] = T1_phys[ i ] / Nx;
+      
+      for( int j = 0; j < Ny; j++ )
+      {
+        T_write[ i ][ j ] = T1_phys[ i ] / Nx;
+      }
+    }
+    
+    // Write netCDF data
+    testWriter.write( t + 1, T_write );
+  }
 
   return 0;
 }
--- visualizeData/readNetCDF.py
+++ visualizeData/readNetCDF.py
@@ -6,12 +6,18 @@ import matplotlib.pyplot as plt
 test = netCDF4.Dataset("/Users/rmoll/Desktop/test_AdvDiff_xy.nc",'r')
 dataMasked = test.variables['data']
 data = dataMasked[:,:,:]
-dataCol = data[0,:,0]
+dataCol0 = data[0,:,0]
+dataCol1 = data[1,:,0]
+dataCol2 = data[2,:,0]
+dataCol3 = data[3,:,0]
 
-plt.figure(1)
-twoDPlot = plt.imshow(data[0,:,:])
-plt.figure(2)
-oneDPlot = plt.plot(dataCol)
+#plt.figure(1)
+#twoDPlot = plt.imshow(data[0,:,:])
+#plt.figure(2)
+oneDPlot0 = plt.plot(dataCol0)
+oneDPlot1 = plt.plot(dataCol1)
+oneDPlot2 = plt.plot(dataCol2)
+oneDPlot3 = plt.plot(dataCol3)
 plt.show()
 #plt.imshow(data[1,:,:])
 #plt.plot(dataCol)
