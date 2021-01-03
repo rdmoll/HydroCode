6c3536a867afc49c794230a4eb5dad13ba298869
rdmoll <ryan.d.moll@gmail.com>
2021-01-02 21:37:24 -0500

    Updated visualization code

--- visualizeData/readNetCDF.py
+++ visualizeData/readNetCDF.py
@@ -1,8 +1,17 @@
 #test
 import numpy as np
 import netCDF4
+import matplotlib.pyplot as plt
 
-test = netCDF4.Dataset("test_class_xy.nc",'r')
-data = test['data'][:][:][:]
-#data = test.variables['data'][:][:][:]
-print(data)
+test = netCDF4.Dataset("/Users/rmoll/Desktop/test_AdvDiff_xy.nc",'r')
+dataMasked = test.variables['data']
+data = dataMasked[:,:,:]
+dataCol = data[0,:,0]
+
+plt.figure(1)
+twoDPlot = plt.imshow(data[0,:,:])
+plt.figure(2)
+oneDPlot = plt.plot(dataCol)
+plt.show()
+#plt.imshow(data[1,:,:])
+#plt.plot(dataCol)
