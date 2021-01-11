deb4af833e7f276ebdc3dec63144db965f3b6c4c
rdmoll <ryan.d.moll@gmail.com>
2021-01-10 20:49:57 -0500

    Testing simple advection-diffusion simulation with 2D FFTs

--- main.cpp
+++ main.cpp
@@ -27,23 +27,18 @@ int main( int argc, const char * argv[] )
   // Solve simple equation
   double pi = std::acos(-1.0);
   
-  size_t Nx = 8; //16;
-  size_t Ny = 8; //16;
+  size_t Nx = 16;
+  size_t Ny = 16;
   int nOutX = std::floor( Nx / 2 + 1 );
   int nOutY = std::floor( Ny / 2 + 1 );
-  size_t nSteps = 10;
+  size_t nSteps = 4;
   double fac1 = 0.01;
-  double fac2 = 0.0;
+  double fac2 = 0.001;
   
-  std::vector< double > T0_phys( Nx, 0.0 );
-  for( int i = 0; i < Nx; i++ )
-  {
-    T0_phys[ i ] = std::cos( i * ( 2 * pi / Nx ) );
-  }
-  
-  std::vector< double > T1_phys( Nx, 0.0 );
-  std::vector< std::complex< double > > T_spec( nOutX, std::complex< double >( 0.0, 0.0 ) );
-  std::vector< std::vector< double > > T_write( Nx, std::vector< double >( Ny, 0.0 ) );
+  std::vector< std::vector< double > > T0_phys( Nx, std::vector< double >( Ny, 0.0 ) );
+  std::vector< std::vector< double > > T1_phys( Nx, std::vector< double >( Ny, 0.0 ) );
+  std::vector< std::vector< std::complex< double > > >
+    T_spec( Nx, std::vector< std::complex< double > >( nOutY, std::complex< double >( 0.0, 0.0 ) ) );
   
   hydroCode::FourierTransforms fft;
   io::ioNetCDF testWriter( "/Users/rmoll/Desktop/test_AdvDiff_xy.nc", "data", Nx, Ny );
@@ -52,39 +47,37 @@ int main( int argc, const char * argv[] )
   {
     for( int j = 0; j < Ny; j++ )
     {
-      T_write[ i ][ j ] = T0_phys[ i ];
+      T0_phys[ i ][ j ] = std::cos( i * ( 2 * pi / Nx ) );
     }
   }
-  testWriter.write( 0, T_write );
+  testWriter.write( 0, T0_phys );
   
   // Starting time step loop
-  for( size_t t = 0; t < 4; t++ )
+  for( size_t t = 0; t < nSteps; t++ )
   {
-    fft.fft_r2c( T0_phys, T_spec );
+    fft.fft_r2c_2d( ( int ) Nx, ( int ) Ny, T0_phys, T_spec );
     
-    for( size_t j = 0; j < nSteps; j++ )
+    for( int i = 0 ; i < Nx ; i++ )
     {
-      for( size_t i = 1; i < nOutX; i++ )
+      for( int j = 0 ; j < nOutY ; j++ )
       {
-        T_spec[ i ] /= ( 1.0 + fac1 * 2.0 * pi * i * std::complex< double >( 0.0, 1.0 )
-                             + fac2 * 4.0 * pi * pi * i * i );
+        T_spec[ i ][ j ] /= ( 1.0 + fac1 * i * std::complex< double >( 0.0, 1.0 )
+                            + fac2 * 4.0 * pi * pi * i * i );
       }
     }
     
-    fft.fft_c2r( T_spec, T1_phys );
+    fft.fft_c2r_2d( ( int ) Nx, ( int ) Ny, T_spec, T1_phys );
     
     for( int i = 0; i < Nx; i++ )
     {
-      T0_phys[ i ] = T1_phys[ i ] / Nx;
-      
       for( int j = 0; j < Ny; j++ )
       {
-        T_write[ i ][ j ] = T1_phys[ i ] / Nx;
+        T0_phys[ i ][ j ] = T1_phys[ i ][ j ] / ( Nx * Ny );
       }
     }
     
     // Write netCDF data
-    testWriter.write( t + 1, T_write );
+    testWriter.write( t + 1, T0_phys );
   }
   
   return 0;
