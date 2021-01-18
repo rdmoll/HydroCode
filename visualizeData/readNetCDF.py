4cfe80cac6d3478c230a50635323441c6f5ef1c0
rdmoll <ryan.d.moll@gmail.com>
2021-01-18 14:07:15 -0500

    Added MPEG generation code

--- visualizeData/readNetCDF.py
+++ visualizeData/readNetCDF.py
@@ -2,7 +2,10 @@
 import numpy as np
 import netCDF4
 import matplotlib.pyplot as plt
+import os
+from PIL import Image
 
+# Read Data
 test = netCDF4.Dataset("/Users/rmoll/Desktop/test_AdvDiff_xy.nc",'r')
 dataMasked = test.variables['data']
 data = dataMasked[:,:,:]
@@ -12,14 +15,22 @@ dataCol2 = data[19,:,0]
 dataCol3 = data[29,:,0]
 dataCol4 = data[39,:,0]
 
-#plt.figure(1)
-#twoDPlot = plt.imshow(data[0,:,:])
-#plt.figure(2)
+# Plot data
+plt.figure(1)
 oneDPlot0 = plt.plot(dataCol0)
 oneDPlot1 = plt.plot(dataCol1)
 oneDPlot2 = plt.plot(dataCol2)
 oneDPlot3 = plt.plot(dataCol3)
 oneDPlot4 = plt.plot(dataCol4)
 plt.show()
-#plt.imshow(data[1,:,:])
-#plt.plot(dataCol)
+
+#if not os.path.exists('images'):
+#    os.makedirs('images')
+#for i in range(0,40):
+#    imageFileName ="./images/screen-" + str(i).zfill(4) + ".tif"
+#    fig = plt.figure()
+#    plt.imshow(data[i,:,:])
+#    fig.savefig(imageFileName)
+#    print(imageFileName)
+#
+#os.system("ffmpeg -r 30 -i ./images/screen-%04d.tif -crf 20 -pix_fmt yuv420p out.mp4")
